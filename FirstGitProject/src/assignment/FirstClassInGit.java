69f54ab0860a451591fa85c72ae1aff1346cd162
jd09590 <jd09590@CEIT2553214X012.ad.georgiasouthern.edu>
2019-08-21 17:05:08 -0400

    Asked for user's last name

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,10 +1,16 @@
 package assignment;
 
+import java.util.Scanner;
+
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello, my name is JD!");
-		
+		System.out.println("Hello my name is JD!");
+		System.out.println("What is your last name");
+		Scanner myInput = new Scanner(System.in);
+		String shaba = myInput.nextLine();
+		System.out.println("Your last name is " + shaba + ".");
+		myInput.close();
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
